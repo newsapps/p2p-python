e8734a7ea9183d2a3fca7a4d14c6ea7db125d1f7
Andy Boyle <andymboyle@gmail.com>
2013-01-08 16:26:09 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 
     packages=find_packages(),
     install_requires=["python-dateutil",
-                      "requests",
+                      "requests", #this needs to be version 0.13.2
                       "iso8601",
                       "clint",
                       "kombu", ],
