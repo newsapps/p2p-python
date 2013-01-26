1a48e495fd976046389f41b9984f206ddfc34249
Ryan Mark <ryan@mrk.cc>
2013-01-26 16:54:08 -0600

    added redis cache support

--- p2p/cache.py
+++ p2p/cache.py
@@ -151,6 +151,7 @@ class NoCache(BaseCache):
     def save_section(self, section, path=None):
         pass
 
+
 try:
     from django.core.cache import cache
     from __init__ import P2P
@@ -239,6 +240,106 @@ try:
                            collection_layout['code'],
                            self.query_to_key(query)])
             cache.set(key, collection_layout)
+
+        def query_to_key(self, query):
+            if query is None:
+                return ''
+
+            return P2P.dict_to_qs(query)
+
+
+except ImportError, e:
+    pass
+
+try:
+    import redis
+    from __init__ import P2P
+
+    class RedisCache(BaseCache):
+        """
+        Cache object for P2P that stores stuff in Redis.
+        """
+        def __init__(self, prefix='p2p', host='localhost', port=6379, db=0):
+            """
+            Takes one parameter, the name of this cache
+            """
+            self.prefix = prefix
+            self.r = redis.StrictRedis(host=host, port=port, db=db)
+
+        def get_content_item(self, slug=None, id=None, query=None):
+            self.content_items_gets += 1
+
+            if slug:
+                key = "_".join([self.prefix, 'content_item',
+                                slug,
+                                self.query_to_key(query)])
+            elif id:
+                key = "_".join([self.prefix, 'content_item',
+                                str(id),
+                                self.query_to_key(query)])
+            else:
+                raise TypeError("get_content_item() takes either a slug or "
+                                "id keyword argument")
+            ret = self.r.get(key)
+            if ret:
+                self.content_items_hits += 1
+            return ret
+
+        def save_content_item(self, content_item, query=None):
+            key = "_".join([self.prefix, 'content_item',
+                            content_item['slug'],
+                            self.query_to_key(query)])
+            self.r.set(key, content_item)
+
+            key = "_".join([self.prefix, 'content_item',
+                            str(content_item['id']),
+                            self.query_to_key(query)])
+            self.r.set(key, content_item)
+
+        def get_collection(self, slug=None, id=None, query=None):
+            self.collections_gets += 1
+
+            if slug:
+                key = "_".join([self.prefix, 'collection',
+                                slug,
+                                self.query_to_key(query)])
+            elif id:
+                key = "_".join([self.prefix, 'collection',
+                                str(id), self.query_to_key(query)])
+            else:
+                raise TypeError("get_collection() takes either a slug or id keyword argument")
+            ret = self.r.get(key)
+            if ret:
+                self.collections_hits += 1
+            return ret
+
+        def save_collection(self, collection, query=None):
+            key = "_".join([self.prefix, 'collection',
+                            collection['code'],
+                            self.query_to_key(query)])
+            self.r.set(key, collection)
+
+            key = "_".join([self.prefix, 'collection',
+                            str(collection['id']),
+                            self.query_to_key(query)])
+            self.r.set(key, collection)
+
+        def get_collection_layout(self, slug, query=None):
+            self.collection_layouts_gets += 1
+
+            key = "_".join([self.prefix, 'collection_layout',
+                            slug, self.query_to_key(query)])
+            ret = self.r.get(key)
+            if ret:
+                ret['code'] = slug
+                self.collection_layouts_hits += 1
+            return ret
+
+        def save_collection_layout(self, collection_layout, query=None):
+            key = "_".join([self.prefix, 'collection_layout',
+                           collection_layout['code'],
+                           self.query_to_key(query)])
+            self.r.set(key, collection_layout)
 
         def query_to_key(self, query):
             if query is None:
