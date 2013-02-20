554876c608d5a1ab32931e1ee85708196eb89f09
Ryan Mark <ryan@mrk.cc>
2013-02-20 09:14:04 -0600

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="p2p",
-    version="1.1",
+    version="1.2",
 
     packages=find_packages(),
     install_requires=["python-dateutil",
