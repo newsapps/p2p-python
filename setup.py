f36ae3ca957e1f3e2a0dfa5121e00b46f96f8b42
Jennifer Lindner <jelindner@tribune.com>
2013-09-06 11:38:26 -0500

    merging empty dictionary fix to put and post_json methods, bumping version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="p2p",
-    version="1.3.5",
+    version="1.4.0",
 
     packages=find_packages(),
     install_requires=["python-dateutil",
