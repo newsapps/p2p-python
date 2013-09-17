bda63b17605f4d2a90a3ad8884e5268c2087415e
Abraham Epton <abraham.epton@gmail.com>
2013-09-17 15:43:50 -0500

    Incrementing version number for new sections code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="p2p",
-    version="1.4.0",
+    version="1.4.1",
 
     packages=find_packages(),
     install_requires=["python-dateutil",
