d9baa0927e41577351a0fd068384f66eb3b1c2ad
Abraham Epton <abraham.epton@gmail.com>
2013-09-18 12:03:13 -0500

    Incrementing version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="p2p",
-    version="1.4.2",
+    version="1.4.3",
 
     packages=find_packages(),
     install_requires=["python-dateutil",
