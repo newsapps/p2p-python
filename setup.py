a0a126b2e1f4c2669e1b97a37bd9ec884a670c2a
Ryan Mark <ryan@mrk.cc>
2013-12-16 14:37:20 -0600

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="p2p-tribune",
-    version="1.5.5",
+    version="1.5.6",
 
     packages=find_packages(),
     install_requires=["python-dateutil",
