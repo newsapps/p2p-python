a34606d8745288c79e7331417e31858df9ae5d66
palewire <ben.welsh@gmail.com>
2015-07-28 17:23:04 -0700

    A little cleanup since we are going our own way

--- setup.py
+++ setup.py
@@ -1,37 +1,20 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
 
 setup(
-    name="p2p-tribune",
+    name="p2p-latimes",
     version="1.5.8",
-
     packages=find_packages(),
-    install_requires=["python-dateutil",
-                      "requests>=2.0",
-                      "iso8601",
-                      "pytz",
-                    ],
-    entry_points={
-        'console_scripts': [
-            'p2pci = p2p.command:content_item_cli',
-        ],
-    },
+    install_requires=(
+        "python-dateutil",
+        "requests",
+        "iso8601",
+        "pytz",
+    ),
     test_suite='p2p.tests',
-
-    # project info
-    author="Ryan Mark, Tribune News Applications",
-    author_email="newsapps@tribune.com",
-    description="Tribune Content Services API wrapper",
-    long_description="Python wrapper for the Tribune Corp. Content Services API. "
-                "The CMS is also known as \"Power to the Producers,\" hence "
-                "the \"p2p\" name you'll see everywhere.",
-    url="http://github.com/newsapps/p2p-python",
+    author="Tribune News Applications, Los Angeles Times Data Desk",
+    author_email="datadesk@latimes.com",
+    description="Tribune P2P API wrapper",
+    long_description="Python wrapper for API at P2P, the Tribune Publishing CMS",
+    url="http://github.com/datadesk/p2p-python",
     license="MIT",
-    keywords=['Development Status :: 4 - Beta',
-              'License :: OSI Approved :: MIT License',
-              'Operating System :: OS Independent',
-              'Programming Language :: Python',
-              'Topic :: Internet',
-              ],
 )
